110e9ae307084507e8c3ece1a3345f6bc74063ce
humback <humback@gmail.com>
2017-11-28 00:05:21 +0100

    file list

--- app/Http/Controllers/ModuleA6CController.php
+++ app/Http/Controllers/ModuleA6CController.php
@@ -31,14 +31,20 @@ class ModuleA6CController extends Controller
 
     public function getPhoto(Request $request,$imagename)
 	{
+		
+		foreach (glob("Public\Modules\A6C/*.*") as $filename) {
+    echo "$filename size " . filesize($filename) . "\n";
+}
+		
+		
 	  //$dstpath=realpath(base_path('/')).'\Public\Modules\A6C\\';
-	  $dstpath='Public\Modules\A6C';
+	//  $dstpath='Public\Modules\A6C';
        //echo $dstpath.$imagename;
-        if(file_exists($dstpath.$imagename.'.jpg'))
+     //   if(file_exists($dstpath.$imagename.'.jpg'))
 		 // if(file_exists('Public\Modules\A6C\\'.$imagename.'.jpg'))
-            return redirect('/Modules/A6C/'.$imagename.'.jpg');
-        else
-           return redirect('/Modules/A6C/'.'null.jpg');
+    //        return redirect('/Modules/A6C/'.$imagename.'.jpg');
+    ////    else
+        //   return redirect('/Modules/A6C/'.'null.jpg');
      
     }
 
