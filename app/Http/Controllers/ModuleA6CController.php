25847b109b1b3d0f3011d7120513aae38fe0f20b
humback <humback@gmail.com>
2017-11-27 23:34:08 +0100

    poprawiona sciezka zapisu

--- app/Http/Controllers/ModuleA6CController.php
+++ app/Http/Controllers/ModuleA6CController.php
@@ -58,7 +58,8 @@ class ModuleA6CController extends Controller
         // echo $request->input("filename").'<br>';
          echo 'Save   '.$imagename.'.jpg  OK!'.'<br>';
 
-          $photoFile->move($dstpath, $imagename.'.jpg');
+         // $photoFile->move($dstpath, $imagename.'.jpg');
+		$photoFile->move('Public\Modules\A6C', $imagename.'.jpg');
 
      }
      else
