3a0ecc97690bcdf20bf0d6be78ce49fc341eaad6
humback <humback@gmail.com>
2017-11-27 22:24:12 +0100

    get photo

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,7 +8,7 @@
 $router->get('/', function () {
     return view('welcome');
 });
-
+/**
 $router->post('/Modules/A6C/Photo/{imagename}', function () {
     return view('welcome_post');
 });
@@ -16,10 +16,11 @@ $router->post('/Modules/A6C/Photo/{imagename}', function () {
 $router->get('/Modules/A6C/{imagename}', function () {
     return view('welcome_get');
 });
-//$router->post('/Modules/A6C/Photo/{imagename}','ModuleA6CController@postPhoto');
+*/
+$router->post('/Modules/A6C/Photo/{imagename}','ModuleA6CController@postPhoto');
 //$router->get('/Modules/A6C/Camera/{imagename}','ModuleA6CController@getPhoto');
 //$router->get('/Modules/A6C/{imagename}','ModuleA6CController@getPhoto');
-
+$router->get('/Modules/A6C/Photo/{imagename}','ModuleA6CController@postPhoto');
 $router->group([
     'middleware'=> ['wechat'],
     'prefix'    => '/wechat'
